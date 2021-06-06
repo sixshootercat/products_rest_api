7199eadf9c8dd58e7ff3c1462fbf2853d18dc656
Kevin Ruhl <kevin.ruhl92@gmail.com>
2021-06-06 00:55:15 -0300

    Adds better documentation and comments to delete handler. Also handles error in case deleting from db fails and outputs message

--- handlers/delete.go
+++ handlers/delete.go
@@ -13,16 +13,28 @@ import (
 //  404: errorResponse
 //  501: errorResponse
 
-// RemoveProduct handles DELETE requests and removes items from the database
-func(p *Products) RemoveProduct(rw http.ResponseWriter, r *http.Request) {
-	id := GetProductID(r)
+// Delete handles DELETE requests and removes items from the database
+func (p *Products) Delete(rw http.ResponseWriter, r *http.Request) {
+	id := getProductID(r)
 
-	p.l.Println("handle DELETE product", id)
+	p.l.Println("[DEBUG] deleting record id", id)
 
 	err := data.DeleteProduct(id)
 	if err == data.ErrProductNotFound {
 		p.l.Println("[ERROR] deleting record id does not exist")
-		http.Error(rw, "Product not found", http.StatusNotFound)
+
+		rw.WriteHeader(http.StatusNotFound)
+		data.ToJSON(&GenericError{Message: err.Error()}, rw)
 		return
 	}
+
+	if err != nil {
+		p.l.Println("[ERROR] deleting record", err)
+
+		rw.WriteHeader(http.StatusInternalServerError)
+		data.ToJSON(&GenericError{Message: err.Error()}, rw)
+		return
+	}
+
+	rw.WriteHeader(http.StatusNoContent)
 }
